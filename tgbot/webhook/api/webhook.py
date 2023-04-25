b8371fc3a30bf264bc8e9edb86953d30e1671498
b1n4r9 <lex.sd6@gmail.com>
2023-04-25 18:51:11 +0300

    add file upload at fastapi to minio

--- tgbot/webhook/api/webhook.py
+++ tgbot/webhook/api/webhook.py
@@ -1,12 +1,36 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, UploadFile
 from tgbot.config import get_config
 from tgbot.webhook.schemas import WebhookRequest
 from tgbot.models.chat_tg import ChatTG
 from tgbot.models.service import Service
+from tgbot.storage import get_storage_client
+from os.path import getsize
+import uuid
 
 router = APIRouter()
 
 config = get_config()
+
+@router.post(f'/files/{config.wh.key}')
+@router.post(f'/files/{config.wh.key}/')
+async def upload_file(file: UploadFile):
+    mc = get_storage_client()
+    
+    # Check if the bucket exists; if not, create it
+    if not mc.bucket_exists(config.storage.bucket):
+        mc.make_bucket(config.storage.bucket)
+    
+    # Save the file to Minio S3
+    file_name = f"{uuid.uuid4()}_{file.filename}"
+    mc.put_object(
+        config.storage.bucket, file_name, file.file, file.size
+    )
+    
+    # Generate public link and reply it to the user
+    presigned_url = mc.presigned_get_object(config.storage.bucket, file_name)
+    
+    return {'link': presigned_url}
+    
 
 @router.get(f'/{config.wh.key}')
 @router.get(f'/{config.wh.key}/')
